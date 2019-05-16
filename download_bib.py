c22fad8bcf5a96381783b6563bcd567c0da421aa
Julio Vega <juliovhz@gmail.com>
2019-05-16 23:24:57 +0100

    Add Jonathan's papers and increase wait to download dblp

--- download_bib.py
+++ download_bib.py
@@ -9,7 +9,7 @@ with  open(path,'r') as file:
     for id in file.readlines():
         urllib.request.urlretrieve(id, f'bibliography/bib/{file_name}.bib')
         file_name += 1
-        time.sleep(5) # Pause to avoid dblp injecting random papers
+        time.sleep(7) # Pause to avoid dblp injecting random papers
 
 for bib in Path("bibliography/bib/").glob("*.bib"):
     subprocess.call(f'academic import --bibtex {bib}', shell = True)
