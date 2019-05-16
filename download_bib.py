b900ecf2f0c8004dc38f3e084842d56487be3105
Julio Vega <juliovhz@gmail.com>
2019-05-16 22:42:23 +0100

    Automatically download and import bibliography

---
+++ download_bib.py
@@ -0,0 +1,15 @@
+import urllib.request
+from pathlib import Path
+import subprocess
+import time
+
+path = "bibliography/dblp-ids.txt"
+with  open(path,'r') as file:
+    file_name = 1
+    for id in file.readlines():
+        urllib.request.urlretrieve(id, f'bibliography/bib/{file_name}.bib')
+        file_name += 1
+        time.sleep(5) # Pause to avoid dblp injecting random papers
+
+for bib in Path("bibliography/bib/").glob("*.bib"):
+    subprocess.call(f'academic import --bibtex {bib}', shell = True)
